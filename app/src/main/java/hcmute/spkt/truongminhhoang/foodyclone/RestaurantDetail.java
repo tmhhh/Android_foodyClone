9376cce1b67c94ac6ecc4d9d34d71065b7c52b0c
JoeyLe <joey.le@contemi.com>
2022-05-04 22:34:44 +0700

    Add restaurant detail page

---
+++ app/src/main/java/hcmute/spkt/truongminhhoang/foodyclone/RestaurantDetail.java
@@ -0,0 +1,41 @@
+package hcmute.spkt.truongminhhoang.foodyclone;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import hcmute.spkt.truongminhhoang.foodyclone.databinding.ActivityRestaurantDetailBinding;
+
+public class RestaurantDetail extends AppCompatActivity {
+
+    private ActivityRestaurantDetailBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityRestaurantDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
