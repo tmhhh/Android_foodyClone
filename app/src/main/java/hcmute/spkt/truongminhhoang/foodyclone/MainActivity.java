8fa22ee255b3c332afe5f07668c2a082b86deb39
Hoàng Trương Minh <tmhhh@tmhhh.local>
2022-05-04 07:49:11 +0700

    first commit

---
+++ app/src/main/java/hcmute/spkt/truongminhhoang/foodyclone/MainActivity.java
@@ -0,0 +1,52 @@
+package hcmute.spkt.truongminhhoang.foodyclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation= (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        replaceFragment(new HomeFragment());
+
+        navigation.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.homeFragment:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.historyFragment:
+                    replaceFragment(new HistoryFragment());
+                    break;
+                case R.id.profileFragment:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+    };
+
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
