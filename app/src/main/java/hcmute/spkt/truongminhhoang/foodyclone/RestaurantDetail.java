91b010c250f6b621837e9cd6523bc85230455481
Huy Le <huyle29092000@gmail.com>
2022-05-05 22:08:38 +0700

    Complete Restaurant Detail page interface

--- app/src/main/java/hcmute/spkt/truongminhhoang/foodyclone/RestaurantDetail.java
+++ app/src/main/java/hcmute/spkt/truongminhhoang/foodyclone/RestaurantDetail.java
@@ -29,13 +29,13 @@ public class RestaurantDetail extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
 
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = binding.fab;
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
